576480852e6e50244b77be7548e986fc56022c76
Ian Warburton <ian.warburton3000@gmail.com>
2013-07-07 17:54:12 +0100

    User.cshtml being cached.

---
+++ Web Applications/TeamMentor.CoreLib/TM_AppCode/Schemas/TMMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeamMentor.CoreLib.TM_AppCode.Schemas
+{
+    public class TM_Message
+    {
+        public bool     Status      { get; set; }
+        public bool     Authorized  { get; set; }
+        public Guid     Guid        { get; set; }
+        public string   Message     { get; set; }
+    }
+}
